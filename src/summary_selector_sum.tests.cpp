abab8ca7ab011d56c96e646b3ff432cd6e2a06a2
PaulVogel96 <vogel.paul1996@gmail.com>
2025-02-07 12:24:41 +0100

    clang-format added, additional simplest test case

--- src/summary_selector_sum.tests.cpp
+++ src/summary_selector_sum.tests.cpp
@@ -5,6 +5,19 @@
 #include <doctest/doctest.h>
 
 TEST_SUITE("suse::summary_selector_sum function") {
+  TEST_CASE("Sum one addition") {
+    suse::summary_selector_sum<int> selector("a(b*c)*d", 10, 10);
+    std::string stream = "a3b4";
+
+    for (std::size_t idx = 0; idx < stream.length(); idx += 2) {
+      std::size_t event_type_index = idx;
+      std::size_t event_value_index = idx + 1;
+      auto event_type = stream[event_type_index];
+      int event_value = stream[event_value_index] - '0'; // convert char to int
+      selector.process_event({event_type, event_value, idx / 2});
+    }
+  }
+
   TEST_CASE("Sum function simple") {
     suse::summary_selector_sum<int> selector("a(b*c)*d", 10, 10);
     std::string stream = "a3b4a1b2c5d6";
@@ -16,7 +29,6 @@ TEST_SUITE("suse::summary_selector_sum function") {
       int event_value = stream[event_value_index] - '0'; //convert char to int
       selector.process_event({event_type, event_value, idx / 2});
     }
-    std::cout << std::endl;
     CHECK(selector.sum_of_contained_complete_matches() == 140);
   }
 }
